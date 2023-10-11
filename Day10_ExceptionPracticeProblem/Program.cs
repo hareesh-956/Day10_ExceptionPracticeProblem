01ab81f67853d5d5b50c98b3cce143449b186e22
hareesh <hareesh.vanapalli.888@gmail.com>
2023-10-11 20:31:22 +0530

    [hari]

---
+++ Day10_ExceptionPracticeProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day10_ExceptionPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a person's age:");
+            string age = Console.ReadLine();
+
+            PersonAge.PersonAgeAbility(age);
+            Console.ReadLine();
+        }
+    }
+}
